f0fc777ffb89ee107926fb76585040f7af1f6b4d
Erazul Islam <erazul89@gmail.com>
2023-12-10 01:37:12 +0600

    added

--- src/Pages/Explore/BookingDetails.jsx
+++ src/Pages/Explore/BookingDetails.jsx
@@ -1,7 +1,37 @@
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import axios from "axios";
+import Swal from "sweetalert2";
+
 /* eslint-disable react/prop-types */
 const BookingDetails = ({data}) => {
 
-    const {image,service_name,price,description} = data || {};
+    const {_id, image,service_name,price,description} = data || {};
+
+    const {user} = useContext(AuthContext);
+
+    const handleAdd = () => {
+        if(user && user.email){
+            const cartItem = {
+                menuId: _id,
+                email: user.email,
+                image,service_name,price,description
+            }
+            axios.post('http://localhost:5000/bookings',cartItem)
+            .then(res => {
+                console.log(res.data)
+                if(res.data.insertedId){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${service_name} is requested for booking`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+        }
+    }
 
     return (
         <div>
@@ -11,7 +41,7 @@ const BookingDetails = ({data}) => {
                             <p className="text-[#111430] pt-4 text-2xl font-semibold">{service_name}</p>
                             <p className="text-[#F63E7B] pt-4 text-xl font-semibold">${price}</p>
                             <p className="pt-4">{description}</p>
-                            <button className="btn hover:bg-pink-400 mt-2 bg-[#F63E7B] text-white rounded-md">Request for Booking</button>
+                            <button onClick={handleAdd} className="btn hover:bg-pink-400 mt-2 bg-[#F63E7B] text-white rounded-md">Request for Booking</button>
                         </div>
                     </div>
         </div>
--- src/Shared/ClientDashboard/BookingList/BookingList.jsx
+++ src/Shared/ClientDashboard/BookingList/BookingList.jsx
@@ -1,23 +1,22 @@
+import useCart from "../../../Hooks/useCart";
+
 const BookingList = () => {
+
+    const [cart] = useCart();
+
     return (
         <div>
             <div className="mt-28 w-[1000px] grid grid-cols-2 pl-8 pt-8 pb-8 h-[500px] bg-[#F4F7FC]">
-                <div className="w-[450px] h-[250px] bg-[#FFF]">
-                    <p className="flex justify-between pl-8 pr-8 pt-8">
-                        <img className="w-16 h-16" src="https://i.ibb.co/ypLKCVL/Group-1372.png" alt="" />
-                        <button className="btn hover:bg-pink-400 bg-[#FFE3E3] border-none text-[#FF4545] rounded-md">Pending</button>
-                    </p>
-                    <p className="text-xl pt-4 pl-8 font-bold text-[#111430]">Anti Age Face Treatment</p>
-                    <p className="pl-8 pr-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                </div>
-                <div className="w-[450px] h-[250px] bg-[#FFF]">
-                    <p className="flex justify-between pl-8 pr-8 pt-8">
-                        <img className="w-16 h-16" src="https://i.ibb.co/ypLKCVL/Group-1372.png" alt="" />
-                        <button className="btn hover:bg-pink-400 bg-[#FFE3E3] border-none text-[#FF4545] rounded-md">Pending</button>
-                    </p>
-                    <p className="text-xl pt-4 pl-8 font-bold text-[#111430]">Anti Age Face Treatment</p>
-                    <p className="pl-8 pr-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                </div>
+                {
+                    cart.map(one => <div key={one._id} className="w-[450px] h-[250px] bg-[#FFF]">
+                        <p className="flex justify-between pl-8 pr-8 pt-8">
+                            <img className="w-16 h-16" src={one.image} alt="" />
+                            <button className="btn hover:bg-pink-400 bg-[#FFE3E3] border-none text-[#FF4545] rounded-md">Pending</button>
+                        </p>
+                        <p className="text-xl pt-4 pl-8 font-bold text-[#111430]">{one.service_name}</p>
+                        <p className="pl-8 pr-8">{one.description}</p>
+                    </div>)
+                }
             </div>
         </div>
     );
